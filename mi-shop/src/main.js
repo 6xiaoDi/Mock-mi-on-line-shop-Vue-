76d4c3c0d63dcd0a0423ed1e71c9296a4637f922
Mark Liu <ldtheended@163.com>
2020-08-31 23:28:29 +0800

    Mock设置方式

--- mi-shop/src/main.js
+++ mi-shop/src/main.js
@@ -5,10 +5,17 @@ import VueAxios from "vue-axios";
 import App from './App.vue'
 // import env from './env/env'
 
+const mock = false; // mock开关
+if (mock) {
+	require('./mock/api');
+}
+
 
 // 根据前端的跨域方式做调整（接口代理：接口域名和前端域名一样，jsonp和CROS，需要设置成http形式）
 // 这里是代理方式，如'/api/getUsr'  =>  转发后前缀被干掉'/getUsr'
 // axios.defaults.baseURL = '/api';
+// easy-mock 对应 url
+axios.defaults.baseURL = ' https://www.easy-mock.com/mock/5f4d0651b52a25318ce1f205/mock';
 axios.defaults.timeout = 8000; // 不设置的话，体验很差
 
 // 根据环境变量设置不同的请求地址
@@ -19,7 +26,7 @@ axios.interceptors.response.use(function (response) {
 	let res = response.data;
 	// 成功
 	if (res.status === 0) {
-		return res.data();
+		return res.data;
 	//	未登录
 	}else if (res.status === 10) {
 		// 跳转到登录界面（在vue实例中才能用路由，这里不行）
