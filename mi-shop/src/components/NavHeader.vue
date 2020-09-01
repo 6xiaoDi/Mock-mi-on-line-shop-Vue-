6eb292e07cdd08afd5d798d84e710ec5d4b77a3c
Mark Liu <ldtheended@163.com>
2020-09-01 17:48:45 +0800

    实现顶部条轮廓

--- mi-shop/src/components/NavHeader.vue
+++ mi-shop/src/components/NavHeader.vue
@@ -18,7 +18,29 @@
                 </div>
             </div>
         </div>
-        <div class="nav-header"></div>
+        <div class="nav-header">
+            <div class="container">
+                <div class="header-logo">
+                    <a href="/#/index"></a>
+                </div>
+                <div class="header-menu">
+                    <!--多个下拉区域菜单，因此不要用a，而是div-->
+                    <div class="item-menu">
+                        <span>小米手机</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>Redmi 红米</span>
+                        <div class="children"></div>
+                    </div>
+                    <div class="item-menu">
+                        <span>电视</span>
+                        <div class="children"></div>
+                    </div>
+                </div>
+                <div class="header-search"></div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -29,6 +51,7 @@
 </script>
 
 <style lang="scss" scoped>
+    @import "./../assets/scss/base.scss";
     .header{
         .nav-topbar{
             height: 39px;
@@ -36,10 +59,6 @@
             background-color: #333;
             color: #B0B0B0;
             .container {
-                width: 1226px;
-                /*保证居中*/
-                margin-right: auto;
-                margin-left: auto;
                 display: flex;
                 /*水平方向两边对齐*/
                 justify-content: space-between;
@@ -63,6 +82,68 @@
                         background: url("/imgs/icon-cart-checked.png") no-repeat center;
                         background-size: contain;
                         margin-right: 4px;
+                    }
+                }
+            }
+        }
+        .nav-header{
+            .container{
+                height: 112px;
+                display: flex;
+                justify-content: space-between;
+                align-content: center;
+                .header-logo{
+                    display: inline-block;
+                    width: 55px;
+                    height: 55px;
+                    background-color: #FF6600;
+                    a{
+                        display: inline-block;
+                        /*两张图片完整放入，外部是相框，里面是相册 宽度*2*/
+                        width: 110px;
+                        height: 55px;
+                        /*  scss生成伪类  */
+                        &:before{
+                            content: "";
+                            display: inline-block;
+                            width: 55px;
+                            height: 55px;
+                            background: url("/imgs/mi-logo.png") no-repeat center;
+                            background-size: contain;
+                            transition: margin .2s;
+                        }
+                        &:after{
+                            /*content是占位的，必须加*/
+                            content: "";
+                            display: inline-block;
+                            width: 55px;
+                            height: 55px;
+                            background: url("/imgs/mi-home.png") no-repeat center;
+                            background-size: contain;
+                        }
+                        &:hover:before{
+                            margin-left: -55px;
+                            /*指定margin进行过渡*/
+                            transition: margin .2s;
+                        }
+                    }
+                }
+                .header-menu{
+                    display: inline-block;
+                    width: 200px;
+                    padding-left: 209px;
+                    .item-menu{
+                        display: inline-block;
+                        color: #333333;
+                        font-weight: bold;
+                        font-size: 16px;
+                        line-height: 112px;
+                        span{
+                            cursor: pointer;
+                        }
+                        &:hover{
+                            /*展示子菜单*/
+                        }
                     }
                 }
             }
