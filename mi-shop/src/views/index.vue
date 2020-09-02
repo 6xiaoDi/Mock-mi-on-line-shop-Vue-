8bbd91af461191383e8b4e7fad6fef1d46790e16
Mark Liu <ldtheended@163.com>
2020-09-02 16:30:17 +0800

    轮播功能实现

--- mi-shop/src/views/index.vue
+++ mi-shop/src/views/index.vue
@@ -1,19 +1,101 @@
 <template>
-    <div>
+    <div class="index">
+        <div class="container">
+            <div class="swiper-box">
+                <swiper :options="swiperOptions">
+                    <swiper-slide v-for="(item,index) in slideList" :key='index'>
+                        <!-- 每张图可以跳转 -->
+                        <a :href="'/#/product' + item.id"><img :src="item.img"></a>
+                    </swiper-slide>
+                    <div class="swiper-pagination"  slot="pagination"></div>
+                    <div class="swiper-button-prev" slot="button-prev"></div>
+                    <div class="swiper-button-next" slot="button-next"></div>
+                </swiper>
+            </div>
+            <div class="ads-box"></div>
+            <div class="product-box"></div>
+        </div>
         <ServiceBar></ServiceBar>
     </div>
 </template>
 
 <script>
 	import ServiceBar from "../components/ServiceBar";
+	// import 'swiper/dist/css/swiper.css'
+	import 'swiper/swiper-bundle.css'
+	import { swiper, swiperSlide } from 'vue-awesome-swiper'
 	export default {
 		name: "index",
 		components:{
+			swiper,
+			swiperSlide,
 			ServiceBar
-		}
+		},
+        data(){
+			return {
+				swiperOptions:{
+					autoplay:true,
+                    // 循环点击播放
+                    loop:true,
+                    // 动画
+					effect : "cube",
+					cubeEffect: {
+						slideShadows: true,
+						shadow: true,
+						shadowOffset: 100,
+						shadowScale: 0.6
+					},
+                    // 分页器
+					pagination:{
+						el:'.swiper-pagination',
+                        clickable:true
+					},
+                    // 前进和后退按钮
+					navigation:{
+						nextEl:'.swiper-button-next',
+						prevEl:'.swiper-button-prev',
+					},
+                },
+                // 轮播图循环列表(暂时是静态的)
+                slideList:[
+	                {
+		                id:'42',
+		                img:'/imgs/slider/slide-1.jpg'
+	                },
+	                {
+		                id:'45',
+		                img:'/imgs/slider/slide-2.jpg'
+	                },
+	                {
+		                id:'46',
+		                img:'/imgs/slider/slide-3.jpg'
+	                },
+                    // 剩下两张无需跳转
+	                {
+		                id:'',
+		                img:'/imgs/slider/slide-4.jpg'
+	                },
+	                {
+		                id:'',
+		                img:'/imgs/slider/slide-5.jpg'
+	                }
+                ]
+            }
+        }
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+    //插件样式覆盖
+    .index{
+        .swiper-box {
+            .swiper-container{
+                height: 451px;
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+        }
+    }
 </style>
