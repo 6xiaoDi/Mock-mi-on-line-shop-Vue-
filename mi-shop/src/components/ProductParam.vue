f452c88c36f9dfe7bc182d5d5afa1c91691241bd
Mark Liu <ldtheended@163.com>
2020-09-09 00:00:27 +0800

    产品站参数组件实现

---
+++ mi-shop/src/components/ProductParam.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="nav-bar">
+        <div class="container">
+            <div class="pro-title">
+                小米8
+            </div>
+            <div class="pro-param">
+                <a href="javascript:;">概述</a><span>|</span>
+                <a href="javascript:;">参数</a><span>|</span>
+                <a href="javascript:;">用户评价</a>
+                <!--按钮：使用插槽，由父级传入（定制），方便其他页面复用-->
+                <slot name="buy"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+	export default{
+		name:'nav-bar',
+		props:{
+			title:String
+		}
+	}
+</script>
+<style lang="scss">
+    @import './../assets/scss/config.scss';
+    @import './../assets/scss/mixin.scss';
+    .nav-bar{
+        height:70px;
+        line-height:70px;
+        border-top:1px solid $colorH;
+        .container{
+            @include flex();
+            .pro-title{
+                font-size:$fontH;
+                color:$colorB;
+                font-weight:bold;
+            }
+            .pro-param{
+                font-size:$fontJ;
+                span{
+                    margin:0 10px;
+                }
+                a{
+                    color:$colorC;
+                }
+            }
+        }
+    }
+</style>
--- mi-shop/src/views/product.vue
+++ mi-shop/src/views/product.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>product</div>
+    <div class="product">
+        <product-param >
+            <!--插槽最近的vue3.0语法-->
+            <template v-slot:buy>
+                <button class="btn">立即购买</button>
+            </template>
+        </product-param>
+    </div>
 </template>
-
 <script>
-    export default {
-        name: "product"
-    }
+	import ProductParam from './../components/ProductParam'
+	export default{
+		name:'product',
+		components:{
+			ProductParam
+		}
+	}
 </script>
-
-<style scoped>
-
+<style lang="scss">
+    @import './../assets/scss/mixin.scss';
+    .product{
+        button{
+            margin-left:10px;
+        }
+    }
 </style>
