23d4c56565cfeb1837cd9e5692b5f37359baa2ee
Mark Liu <ldtheended@163.com>
2020-09-13 17:08:40 +0800

    完成确认订单页面的地址和商品数据加载

--- mi-shop/src/views/order.vue
+++ mi-shop/src/views/order.vue
@@ -29,13 +29,13 @@
 		},
         mounted() {
             let path = this.$route.path;
-            if (path === 'order/confirm'){
+            if (path === '/order/confirm'){
                 this.title = '订单确认';
                 this.tip = '请认真填写收货地址';
-            } else if (path === 'order/list') {
+            } else if (path === '/order/list') {
 	            this.title = '订单列表';
 	            this.tip = '谨防钓鱼链接或诈骗电话，了解更多';
-            } else if (path === 'order/pay') {
+            } else if (path === '/order/pay') {
 	            this.title = '订单支付';
 	            this.tip = '谨防钓鱼链接或诈骗电话，了解更多';
             }
