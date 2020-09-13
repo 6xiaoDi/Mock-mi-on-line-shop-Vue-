508d976cf3f8263962ac60792b98914dd73e3142
Mark Liu <ldtheended@163.com>
2020-09-13 11:47:49 +0800

    购物车商品更新和购物车单选状态与删除

--- mi-shop/src/views/cart.vue
+++ mi-shop/src/views/cart.vue
@@ -19,7 +19,7 @@
                     <ul class="cart-item-list">
                         <li class="cart-item" v-for="(item) in list" :key="item.productId">
                             <div class="item-check">
-                                <span class="checkbox" :class="{'checked':item.productSelected}"  ></span>
+                                <span class="checkbox" :class="{'checked':item.productSelected}"  @click="updateCart(item)"></span>
                             </div>
                             <div class="item-name">
                                 <img v-lazy="item.productMainImage" alt="">
@@ -28,13 +28,13 @@
                             <div class="item-price">{{item.productPrice}}</div>
                             <div class="item-num">
                                 <div class="num-box">
-                                    <a href="javascript:;">-</a>
+                                    <a href="javascript:;" @click="updateCart(item,'-')">-</a>
                                     <span>{{item.quantity}}</span>
-                                    <a href="javascript:;"  >+</a>
+                                    <a href="javascript:;"  @click="updateCart(item,'+')">+</a>
                                 </div>
                             </div>
                             <div class="item-total">{{item.productTotalPrice}}</div>
-                            <div class="item-del" ></div>
+                            <div class="item-del" @click="delProduct(item)"></div>
                         </li>
                     </ul>
                 </div>
@@ -82,6 +82,39 @@
 				this.axios.get('/carts').then((res)=>{
 					this.renderData(res);
 				})
+			},
+			// 更新购物车数量和购物车单选状态
+			updateCart(item,type){
+				let quantity = item.quantity,
+					selected = item.productSelected;
+				if(type == '-'){
+					if(quantity == 1){
+						alert('商品至少保留一件');
+						return;
+					}
+					--quantity;
+				}else if(type == '+'){
+					if(quantity > item.productStock){
+						alert('购买数量不能超过库存数量');
+						return;
+					}
+					++quantity;
+				}else{
+					selected = !item.productSelected;
+				}
+				this.axios.put(`/carts/${item.productId}`,{
+					quantity,
+					selected
+				}).then((res)=>{
+					this.renderData(res);
+				})
+			},
+			// 删除购物车商品
+			delProduct(item){
+				this.axios.delete(`/carts/${item.productId}`).then((res)=>{
+					alert('删除成功');
+					this.renderData(res);
+				});
 			},
 			// 控制全选功能
 			toggleAll(){
