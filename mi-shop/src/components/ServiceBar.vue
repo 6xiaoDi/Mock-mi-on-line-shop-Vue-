52990db31c95bdaf50c062047f7c8d3a2c37812b
Mark Liu <ldtheended@163.com>
2020-09-15 14:10:37 +0800

    修复顶部servicebar不在安全容器的问题！

--- mi-shop/src/components/ServiceBar.vue
+++ mi-shop/src/components/ServiceBar.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="service">
-        <div class="container"></div>
-        <ul>
-            <li><span class="icon-setting"></span>预约维修服务</li>
-            <li><span class="icon-7day"></span>7天无理由退货</li>
-            <li><span class="icon-15day"></span>15天免费换货</li>
-            <li><span class="icon-post"></span>满150元包邮</li>
-        </ul>
+        <div class="container">
+            <ul>
+                <li><span class="icon-setting"></span>预约维修服务</li>
+                <li><span class="icon-7day"></span>7天无理由退货</li>
+                <li><span class="icon-15day"></span>15天免费换货</li>
+                <li><span class="icon-post"></span>满150元包邮</li>
+            </ul>
+        </div>
     </div>
 </template>
 
