b10a867f0b3b1157c691c063fdb2b15a4da0e9ca
Mark Liu <ldtheended@163.com>
2020-09-15 11:45:55 +0800

    修复从订单支付跳到订单列表，不会调用order组件的mounted生命周期的问题。

--- mi-shop/src/views/order.vue
+++ mi-shop/src/views/order.vue
@@ -1,47 +1,47 @@
 <template>
     <div>
-        <OrderHeader :title="title">
-            <template v-slot:tip>
-                <span>{{tip}}</span>
-            </template>
-        </OrderHeader>
+<!--        <OrderHeader :title="title">-->
+<!--            <template v-slot:tip>-->
+<!--                <span>{{tip}}</span>-->
+<!--            </template>-->
+<!--        </OrderHeader>-->
         <router-view></router-view>
         <service-bar></service-bar>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
-    import OrderHeader from "../components/OrderHeader";
+    // import OrderHeader from "../components/OrderHeader";
 	import ServiceBar from './../components/ServiceBar'
 	import NavFooter from './../components/NavFooter'
 	export default{
 		name:'order',
 		data(){
 			return {
-				title:'',
-				tip:''  // 提示
+				// title:'',
+				// tip:''  // 提示
 			}
 		},
 		components:{
-			OrderHeader,
+			// OrderHeader,
 			ServiceBar,
 			NavFooter
 		},
         mounted() {
-            let path = this.$route.path;
-            if (path === '/order/confirm'){
-                this.title = '订单确认';
-                this.tip = '请认真填写收货地址';
-            } else if (path === '/order/list') {
-	            this.title = '订单列表';
-	            this.tip = '谨防钓鱼链接或诈骗电话，了解更多';
-            } else if (path === '/order/pay') {
-	            this.title = '订单支付';
-	            this.tip = '谨防钓鱼链接或诈骗电话，了解更多';
-            }else{
-	            this.title = '订单支付';
-	            this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
-            }
+            // let path = this.$route.path;
+            // if (path === '/order/confirm'){
+            //     this.title = '订单确认';
+            //     this.tip = '请认真填写收货地址';
+            // } else if (path === '/order/list') {
+	        //     this.title = '订单列表';
+	        //     this.tip = '谨防钓鱼链接或诈骗电话，了解更多';
+            // } else if (path === '/order/pay') {
+	        //     this.title = '订单支付';
+	        //     this.tip = '谨防钓鱼链接或诈骗电话，了解更多';
+            // }else{
+	        //     this.title = '订单支付';
+	        //     this.tip = '请谨防钓鱼链接或诈骗电话，了解更多';
+            // }
         }
 	}
 </script>
--- mi-shop/src/views/orderPay.vue
+++ mi-shop/src/views/orderPay.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="order-pay">
+        <order-header title="订单支付">
+            <template v-slot:tip>
+                <span>请谨防钓鱼链接或诈骗电话，了解更多</span>
+            </template>
+        </order-header>
         <div class="wrapper">
             <div class="container">
                 <div class="order-wrap">
